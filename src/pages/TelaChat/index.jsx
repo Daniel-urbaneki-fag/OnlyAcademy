b48a9b5903816361d02c94dc863dd521759e318b
Daniel urbaneki <daniel_urba@hotmail.com>
2024-05-11 16:24:43 -0300

    Alterando forma de criação e de telas e implementando bottom navigator

---
+++ src/pages/TelaChat/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export const TelaChat = () => {
+    return (
+        <View>
+            <Text>Tela Chat Aqui</Text>
+        </View>
+    )
+}
---
+++ src/pages/TelaHome/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export const TelaHome = () => {
+    return (
+        <View>
+            <Text>Tela Home Aqui</Text>
+        </View>
+    )
+}
